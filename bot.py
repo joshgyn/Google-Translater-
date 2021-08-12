e553ac868996766b37bae936321f7d0a0e881e5f
dadadadd1321 <87093609+feriid12312@users.noreply.github.com>
2021-08-12 19:11:11 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,10 +21,7 @@ app = Client(
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
 	await message.reply_text(text =f"Salam **{message.from_user.first_name }** 🙋 \n\n __Mən tərcüməçi botam 🙆 \n Mən istənilən sözü/cümləni/paraqrafı sənin seçdiyin dilə çevirə bilirəm 🙇 \n İndi mənə tərcümə etmək istədiyin mesajı göndər🙂__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Support 🤖" ,url="https://t.me/menzroyam") ],               [InlineKeyboardButton("Abunə ol🧐", url="https://t.me/menzroyam") ],               [InlineKeyboardButton("Sahibim👩‍💻", url="https://t.me/MenZroyam") ]   ]  ) )
-                  
-@app.on_message(filters.private & filters.command(['help']))
-async def help(client, message):
-	await message.reply_text(text =f"Salam **{message.from_user.first_name }**)  
+                 
 	
 @app.on_message(filters.private & filters.text  )
 async def echo(client, message):
@@ -271,4 +268,8 @@ async def translate_text(bot,update):
        translation = translator.translate(tr_text,dest=cb_data) 
        await update.message.edit(translation.text)
 
+@app.on_message(filters.private & filters.command(['help']))
+async def help(client, message):
+	await message.reply_text(text =f"Salam **{message.from_user.first_name }**)  
+				 
 app.run()
