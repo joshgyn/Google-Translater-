b768d2296d6ebc229298c6acba5be095dab59e6b
dadadadd1321 <87093609+feriid12312@users.noreply.github.com>
2021-08-12 18:47:01 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# @mrjeluxe ©️ coshgyn
+# @menzroyam
 #lntechnical
 import os
 from pyrogram import Client, filters
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Salam **{message.from_user.first_name }** 🙋 \n\n __Mən tərcüməçi botam 🙆 \n Mən istənilən sözü/cümləni/paraqrafı sənin seçdiyin dilə çevirə bilirəm 🙇 \n İndi mənə tərcümə etmək istədiyin mesajı göndər🙂__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Support 🤖" ,url="https://t.me/JLBchat") ],               [InlineKeyboardButton("Abunə ol🧐", url="https://t.me/jlbots") ],               [InlineKeyboardButton("Sahibim👩‍💻", url="https://t.me/sammekkim") ]   ]  ) )
+	await message.reply_text(text =f"Salam **{message.from_user.first_name }** 🙋 \n\n __Mən tərcüməçi botam 🙆 \n Mən istənilən sözü/cümləni/paraqrafı sənin seçdiyin dilə çevirə bilirəm 🙇 \n İndi mənə tərcümə etmək istədiyin mesajı göndər🙂__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Support 🤖" ,url="https://t.me/Etiraf_Story") ],               [InlineKeyboardButton("Abunə ol🧐", url="https://t.me/EtirafStory") ],               [InlineKeyboardButton("Sahibim👩‍💻", url="https://t.me/MenZroyam") ]   ]  ) )
                   
 
 
