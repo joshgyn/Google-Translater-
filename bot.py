05a09540f66beedd534ecc662916009677a85d0a
Je Luxe <78498193+joshgyn@users.noreply.github.com>
2021-08-14 15:11:15 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Salam **{message.from_user.first_name }** 🙋 \n\n __Mən tərcüməçi botam 🙆 \n Mən istənilən sözü/cümləni/paraqrafı sənin seçdiyin dilə çevirə bilirəm 🙇 \n İndi mənə tərcümə etmək istədiyin mesajı göndər🙂__ \n **/help yazaraq daha ətraflı məlumat alın.**",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Etiraf Botumuz 🤖" ,url="https://t.me/EtirafStoryBot?start") ],               [InlineKeyboardButton("Abunə ol🧐", url="https://t.me/EtirafStory") ],               [InlineKeyboardButton("Sahibim👨🏻‍💻", url="https://t.me/MenZroyam") ]  ]  ) )
+	await message.reply_text(text =f"Salam,🙋,Əziz **{message.from_user.first_name }**  \n\n __Mən müxtəlif dillərdə insanlara kömək olmaq üçün yaradılmış tərcüməçi botam. \n Ümid edirəm ki, səndən məndən doğru şəkildə yararlanacaqsan☺ \n **/help yazaraq daha ətraflı məlumat ala bilərsən.**",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Etiraf Botumuz 🤖" ,url="https://t.me/EtirafStoryBot?start") ],               [InlineKeyboardButton("Abunə ol🧐", url="https://t.me/EtirafStory") ],               [InlineKeyboardButton("Sahibim👨🏻‍💻", url="https://t.me/MenZroyam") ]  ]  ) )
 
 @app.on_message(filters.private & filters.command(['help']))
 async def help(client, message):
